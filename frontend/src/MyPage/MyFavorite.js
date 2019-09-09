d064ddd9c9e1be5250e24af8609c45d38fd778e0
PyoSH <pygosky@gmail.com>
2019-09-09 19:28:06 +0900

    즐겨찾기 : 마이페이지부분 대강완료

--- frontend/src/MyPage/MyFavorite.js
+++ frontend/src/MyPage/MyFavorite.js
@@ -1,43 +1,100 @@
 import React, { Component } from 'react';
 import { getAllFavorite } from './MyPageFunction';
+import './MyFavorite.css';
 
 class MyFavorite extends Component {
     state = {
-        favoriteFolder : []
+        favoriteFolder: [],
+        nowPage: 0,
+        favoriteLength: 0
     }
     componentDidMount() {
         const ID = this.props.getID();
         getAllFavorite(ID).then(res=>{
-            this.setState({
-                favoriteFolder : res
-            })
+            if(!res.length !== 0)
+                this.setState({
+                    favoriteFolder : res,
+                    nowPage: 1,
+                    favoriteLength: res.length
+                })
+            else
+                this.setState({
+                    favoriteFolder : res,
+                    favoriteLength: res.length
+                })
         })
     }
-    render() {
-        const { favoriteFolder } = this.state;
-        let folderFile = '' ;
+
+    favOnClick = (e) => {
+        if(e.target.className==="MyFavorite-Selected-Button"){}
+        else{
+            let children = [...e.target.parentNode.children];
+            children.map((child) => {
+                child.className = null;
+                return null;
+            })
+            e.target.className="MyFavorite-Selected-Button";
+            this.setState({
+                nowPage: parseInt(e.target.innerText)
+            });
+        }
+    }
+
+    _renderButton = () => {
+        const { favoriteLength } = this.state;
+        let buttonList = [];
+        for(let i=1; i <= favoriteLength; ++i)
+            buttonList = buttonList.concat(i);
+        let buttonDiv = '';
+        buttonDiv = buttonList.map((list, index) => {
+            if(index === 0)
+                return <button 
+                    className="MyFavorite-Selected-Button"
+                    key={index+1}
+                    onClick={this.favOnClick}
+                    >{list}</button>
+            return <button key={index+1} onClick={this.favOnClick}>{list}</button>;
+        })
+        return buttonDiv;
+    }
+
+    _renderFolder =(favoriteFolder, nowPage) => {
+        let folderFile = '';
         if(favoriteFolder !== null) {
             folderFile = favoriteFolder.map((folder, index) => {
-                let favName = ''
-                // if(folder.favorites[0]) {
-                //     favName = folder.favorites[0].favName;
-                // }
-                if(folder.favorites){
-                    favName = folder.favorites.map((file, index) => {
-                        return <li key={index+1}>{index + 1 }째 파일 {file.favName}</li>
-                    })
+                if((index+1) === nowPage){
+                    let favName = ''
+                    if(folder.favorites){
+                        favName = folder.favorites.map((file, index) => {
+                            return <div className="MyFavorite-File" key={index+1}>{index + 1}. {file.favName}</div>
+                        });
+                    }
+                    return (
+                        <div className="MyFavorite-Files" key = {index +1}>
+                            <div className="MyFavorite-Title">{index + 1}. {folder.favFolderName}</div>
+                            <div>{favName}</div>
+                        </div>
+                    );
                 }
-                return (<div key = {index +1}>
-                <div>{index + 1}째 폴더이름 : {folder.favFolderName}</div>
-                <div> 파일 이름 : {favName}</div>
-                </div>
-                )
+                else
+                    return null;
             })
         }
-     
         return (
             <div>
                 {folderFile}
+            </div>
+        );
+    }
+
+    render() {
+        const { favoriteFolder, nowPage } = this.state;
+        return (
+            <div className="MyFavorite-Window">
+                <div className="MyFavorite-Buttons">
+                    {this._renderButton()}
+                </div>
+                {this._renderFolder(favoriteFolder, nowPage)}
             </div>
         );
     }
