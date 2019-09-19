f553387389d24ae8e50aecabf2679b14eeb0fcdf
문건우 <ansejrrhkd@gmail.com>
2019-09-20 06:28:22 +0900

    깃 이그노어 수정

--- frontend/src/Upload/awsconfig.js
+++
@@ -1,6 +0,0 @@
-export const awsconfig = 
-    {
-        accessKeyId : "AKIAJW3OJXC2ANDBVXKA",
-        secretAccessKey: "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",
-        region : "ap-northeast-2"
-    }
