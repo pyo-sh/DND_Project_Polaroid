ebb105c142fab133aaf6cefa48273d7bb7c5a50f
Choi Jeongeun <cje3080@gmail.com>
2019-09-19 23:22:17 +0900

    유저 검색

--- frontend/src/Main/SearchUser.js
+++ frontend/src/Main/SearchUser.js
@@ -2,8 +2,30 @@ import React, {Component} from 'react';
 import Users from '../Profile/ProfileSmall';
 import jwt_decode from 'jwt-decode';
 import './ContentTop.css';
+import { getAllUser } from './MainFunction';
 
 class SearchUser extends Component {
+    state = {
+        userid: [],
+        searchids: []
+    }
+
+    componentDidMount(){
+        getAllUser().then(res => {
+            this.setState({
+                userid: res
+            })
+            console.log(this.state.userid);
+            this.idContrast();
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) { // 서치 값이 달라지면 다시 contrast 하게
+        if(prevProps.id !== this.props.id){
+          this.idContrast();
+        }
+    }
+
     getID = () => {
         let token = "";
         localStorage.usertoken
@@ -17,18 +39,30 @@ class SearchUser extends Component {
       //indexOf() 사용
       //검색한 단어를 가진 아이디들을 뽑아서 배열에 넣어....
 
-      /*
+
     idContrast = () =>{
-        let searchimages = this.state.images.filter(searchimage => {
-            let temp = searchimage.tag.split(',');
-            return temp.includes(search)
+        let { id } = this.props;
+        let searchids = this.state.userid.filter(searchid => searchid.ID.includes(id))
+        this.setState({
+            searchids
         })
-    } */
+        console.log(searchids)
+        this.props.getUserCount(searchids.length)
+    } 
 
     render() {
         return ( 
         <div className = "Search-User">
-            <Users id = {this.props.id} isMe = {this.props.id === this.getID()} isFollow = {false}/>
+            
+            {this.state.searchids.map((ids) => {
+                
+                /*console.log(typeof(Object.values(ids).toString()))
+                console.log(this.getID())
+                console.log(Object.values(ids).toString() === this.getID())*/
+
+                //id는 Object의 값을 받아옴
+                return(<Users id = {Object.values(ids)} isMe = {Object.values(ids).toString() === this.getID()} isFollow = {false}/>)
+            })}
             
             {/*<Users id = "201712651" isMe = {false} isFollow = {true}/>
             <Users id = "201712651" isMe = {true} isFollow = {true}/>
